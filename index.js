39062da18f8179963ff18b6867034a030f120327
TinyMito <k.ip@me.com>
2023-04-22 00:23:48 -0400

    Update comment

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const breedName = process.argv.slice(2);
 
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
+    // Return if no breed entered or blank or api error
     console.log('Error fetch details:', error);
   } else {
+    // Output the breed data
     console.log(desc);
   }
 });
