e51886abee39a743759e9fbb863097d188087b64
TinyMito <k.ip@me.com>
2023-04-21 23:56:05 -0400

    Add error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,27 @@
 const request = require('request')
 const breedName = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  console.log(typeof body)
+if (breedName.length === 0) {
+  console.log('Please enter a breed name!')
+  return;
+}
 
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  if (error) {
+    console.log(error)
+    return;
+  }
+  if (response) {
+    //console.log(response)
+  }
+  
+  if (body.length <= 2) {
+    console.log('Breed is not found!')
+    return;
+  }
+
+  //console.log(body.length)
+  //console.log(typeof body)
   const data = JSON.parse(body);
   console.log(data);
   console.log(typeof data);
