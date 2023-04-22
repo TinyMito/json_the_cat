15ee1f9d83ce34abc61b290c457220a8cff327f4
TinyMito <k.ip@me.com>
2023-04-21 23:39:48 -0400

    Test datatype

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,5 @@
-const request = require('request');
+const request = require('request')
+
+request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+  console.log(typeof body)
+})
