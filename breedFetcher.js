ad5a202925b9d4ff836dcc028adc1cc2a7ce88c3
TinyMito <k.ip@me.com>
2023-04-21 23:46:34 -0400

    Add dynamic breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,11 @@
 const request = require('request')
+const breedName = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   console.log(typeof body)
+
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
+
 })
